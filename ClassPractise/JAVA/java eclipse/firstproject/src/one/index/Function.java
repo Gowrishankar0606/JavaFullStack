944f07049330d2a59c1b673a97749487d864858a
Gowri shankar <119755034+Gowrishankar0606@users.noreply.github.com>
2024-09-23 16:46:39 +0530

    hlo

---
+++ ClassPractise/JAVA/java eclipse/firstproject/src/one/index/Function.java
@@ -0,0 +1,19 @@
+package one.index;
+
+public class Function {
+	
+	static void Method()
+	{
+		System.out.println("Hai i am GS");
+		
+	}
+public static void main(String[] args)
+{
+	Function gs=new Function();
+	
+	Method();
+	
+}
+
+
+}
---
+++ ClassPractise/JAVA/java eclipse/firstproject/src/one/index/MethodmanyParameter.java
@@ -0,0 +1,22 @@
+package one.index;
+
+public class MethodmanyParameter {
+
+	
+	static void Method(String fname)
+	{
+		System.out.println(fname+" is my name");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MethodmanyParameter GS=new MethodmanyParameter();
+		Method("GS");
+		Method("SNE");
+		Method("Ragu");
+		Method("Sha");
+		Method("Moha");
+		Method("Bha");
+	}
+
+}
