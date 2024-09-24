90d0fbff2b1c53ef083d091301ddc8ce7f47b469
Gowri shankar <119755034+Gowrishankar0606@users.noreply.github.com>
2024-09-24 09:59:36 +0530

    task10

---
+++ Task/src/Task/index/add2umStatic.java
@@ -0,0 +1,21 @@
+package Task.index;
+
+public class add2umStatic {
+
+	public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 20;
+
+        // Call the static method and store the result
+        int result = add(num1, num2);
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+	
+}
